5ff6689a07d1571f3bd879f5287c27d6c3b8a3eb
Stickie <71910589+FieryIceStickie@users.noreply.github.com>
2024-04-25 19:56:38 +1000

    Change path from named arg to positional arg (#38)
    Signed-off-by: stickie <71910589+FieryIceStickie@users.noreply.github.com>

--- src/vipyr_deobf/cli.py
+++ src/vipyr_deobf/cli.py
@@ -100,7 +100,7 @@ def run():
         prog='Vipyr Deobfuscator',
         description='Deobfuscates obfuscated scripts',
     )
-    parser.add_argument('-p', '--path', required=True, help='path to obfuscated file')
+    parser.add_argument('path', help='path to obfuscated file')
     parser.add_argument('-t', '--type', default='auto', help='type of obfuscation used')
     parser.add_argument('-d', '--debug', action='store_true', help='display debug logs (defaults to false)')
     parser.add_argument('-s', '--soft', action='store_true', help='display expected warnings (defaults to false)')
