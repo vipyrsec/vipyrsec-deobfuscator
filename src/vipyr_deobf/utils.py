7679bfed9aafa3a0b038b48bea19f822e07acdf3
stickie <71910589+FieryIceStickie@users.noreply.github.com>
2024-05-20 03:06:32 +1000

    Add pyobfuscate deobf and scanner
    Signed-off-by: stickie <71910589+FieryIceStickie@users.noreply.github.com>

--- src/vipyr_deobf/utils.py
+++ src/vipyr_deobf/utils.py
@@ -3,6 +3,8 @@ import base64
 import operator
 import re
 import zlib
+from ast import Constant
+from collections.abc import Callable
 
 WEBHOOK_REGEX = re.compile(
     r'https?://(?:ptb\.|canary\.)?discord(?:app)?\.com/api(?:/v\d{1,2})?/webhooks/\d{17,21}/[\w-]{68}'
@@ -10,7 +12,6 @@ WEBHOOK_REGEX = re.compile(
 BYTES_WEBHOOK_REGEX = re.compile(
     br'https?://(?:ptb\.|canary\.)?discord(?:app)?\.com/api(?:/v\d{1,2})?/webhooks/\d{17,21}/[\w-]{68}'
 )
-
 
 known_funcs = {
     ('base64', 'b64decode'): base64.b64decode,
@@ -37,3 +38,23 @@ op_dict = {
     ast.Not: operator.not_,
     ast.Invert: operator.invert,
 }
+
+
+class StringCollapser(ast.NodeTransformer):
+    """
+    Collapses Constant(value=str()) to some smaller str()
+    To help with seeing ast code structure
+    """
+
+    def __init__(self, renamer: str | Callable[[str], str] = 'a'):
+        """
+        :param renamer: Either a string to rename everything to, or a function that receives the original
+        variable name as argument and outputs the new name
+        """
+        self.renamer = (lambda _: renamer) if isinstance(renamer, str) else renamer
+
+    def visit_Constant(self, node):
+        match node:
+            case Constant(value=str(value)):
+                return Constant(value=self.renamer(value))
+        return node
